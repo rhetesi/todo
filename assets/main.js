f58b5528587c22aca72b632d1db801143949cecf
rhetesi <rhetesi@gmail.com>
2020-12-08 17:02:58 +0100

    some settings

--- assets/main.js
+++ assets/main.js
@@ -1,2 +1,20 @@
 'use strict';
 
+// show Today
+
+const now = new Date();
+
+function formattedDate(date) {
+  
+  const Options = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  }
+  return date.toLocaleDateString('hu-HU', Options);
+}
+
+const showToday = document.querySelector('.todo__date');
+showToday.textContent = formattedDate(now);
+
