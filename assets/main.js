dc5e9d0fdf439e181ba2c64511f7c6f507154737
rhetesi <rhetesi@gmail.com>
2020-12-08 20:05:44 +0100

    Date basics

--- assets/main.js
+++ assets/main.js
@@ -5,14 +5,14 @@
 const now = new Date();
 
 function formattedDate(date) {
-  
-  const Options = {
-    weekday: 'long',
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-  }
-  return date.toLocaleDateString('hu-HU', Options);
+
+    const Options = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+    }
+    return date.toLocaleDateString('hu-HU', Options);
 }
 
 const showToday = document.querySelector('.todo__date');
