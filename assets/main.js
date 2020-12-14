4569664afc44af5d51e5b218740fe986de8213ce
rhetesi <rhetesi@gmail.com>
2020-12-14 22:32:46 +0100

    localDB -től folytatni

--- assets/main.js
+++ assets/main.js
@@ -18,3 +18,18 @@ function formattedDate(date) {
 const showToday = document.querySelector('.todo__date');
 showToday.textContent = formattedDate(now);
 
+// localStorage handler
+
+const localDB = {
+    setItem(key, value) {
+        value = JSON.stringify(value);
+        localStorage(key, value);
+    },
+    getItem(key) {
+        const value = localStorage.getItem(key);
+        return JSON.parse(value);
+    },
+    removeItem(key) {
+        localStorage.removeItem(key);
+    }
+};
